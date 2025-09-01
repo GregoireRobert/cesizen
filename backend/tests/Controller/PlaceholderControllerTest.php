034501daa550faf10af6e1cc27a41009a13502a9
GregoireRobert <123934172+GregoireRobert@users.noreply.github.com>
2025-09-01 13:30:32 +0200

    Create PlaceholderControllerTest.php

---
+++ backend/tests/Controller/PlaceholderControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+final class PlaceholderControllerTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
