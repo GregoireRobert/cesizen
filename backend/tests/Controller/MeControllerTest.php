ba651a89d6fa39a81fb235ebf4d5132aac9476ac
GregoireRobert <123934172+GregoireRobert@users.noreply.github.com>
2025-09-01 13:18:32 +0200

    Update MeControllerTest.php

--- backend/tests/Controller/MeControllerTest.php
+++ backend/tests/Controller/MeControllerTest.php
@@ -1,15 +1,20 @@
-<?php
-
 namespace App\Tests\Controller;
 
+use App\Kernel; // <-- important
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class MeControllerTest extends WebTestCase
+final class MeControllerTest extends WebTestCase
 {
+    // Indique explicitement la classe du Kernel
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
+    }
+
     public function testSomething(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/api/me');
-        $this->assertResponseIsSuccessful();
+        $client->request('GET', '/api/me');
+        $this->assertResponseIsSuccessful(); // ajuste si /api/me nécessite une auth (voir note)
     }
 }
